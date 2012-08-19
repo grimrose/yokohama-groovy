c1ad4b7ead82adba915b23b172651fd4731bd81b
grimrose <grwin7@gmail.com>
2012-08-19 15:02:52 +0900

    add word count specification

---
+++ src/test/groovy/WordCountSpec.groovy
@@ -0,0 +1,28 @@
+import spock.lang.Specification
+/**
+ * Created with IntelliJ IDEA.
+ * User: grimrose
+ * Date: 2012/08/18
+ * Time: 13:40
+ * To change this template use File | Settings | File Templates.
+ */
+class WordCountSpec extends Specification {
+
+
+    def "specification wordCount running" () {
+
+        expect:
+        def base = new File("")
+        println base.absolutePath
+
+        GroovyShell shell = new GroovyShell()
+
+        def script = shell.parse(new File(base.absolutePath, "src/main/groovy/wordCount.groovy"))
+        script.setProperty("args", [ "${base.absolutePath}/src/main/resources/input.txt"])
+
+        def expect = [not: 2, it:2, That: 5, is: 6]
+        assert script.run() == expect
+
+    }
+
+}
