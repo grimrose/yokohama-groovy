c434a4195b32c4ec6c47783149ec91dff6613f44
grimrose <grwin7@gmail.com>
2013-04-21 17:06:11 +0900

    add sample

---
+++ src/main/java/Sample.java
@@ -0,0 +1,12 @@
+public class Sample {
+	
+	public static void main(String[] args) {
+		Sample sample = new Sample();
+		sample.sayHello();
+	}
+	
+	void sayHello() {
+		System.out.print("Hello Java!");
+	}
+	
+}
